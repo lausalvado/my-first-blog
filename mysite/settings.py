521c529ccc8de974ea96ce473ea8882b2b33199f
lausalvado <lsalvado@shalion.com>
2020-10-03 17:02:00 +0200

    mi app

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cj++)rkt0gd05_3mh0&9+2zr29!c3n=s@3!o$&_f=2n!ixo-35'
+SECRET_KEY = 'j&!!gz9*g-e(wwi%(=g3h@-yjc6smob*#-rwh1-j49_7*a6c!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
